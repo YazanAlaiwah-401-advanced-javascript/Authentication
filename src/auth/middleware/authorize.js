9fc077e54ca1455103cb5c480d5f184c3613c3c3
YazanAlaiwah <eng.yazanalaiwah@gmail.com>
2020-06-11 00:26:17 +0300

    add some jsdoc

--- src/auth/middleware/authorize.js
+++ src/auth/middleware/authorize.js
@@ -1,5 +1,9 @@
 'user strict';
 const User = require('../models/users-schema.js');
+/**
+ * @function permission
+ * @param {String} capability the permission the  route need
+ */
 module.exports = (capability)=> (req,res,next) =>{
   let user = req.user;
   if(User.can(capability,user.role)){
